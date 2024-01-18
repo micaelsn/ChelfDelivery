d5a1940604956698763f60136739266795ef2261
Micael Santos Nascimento <micaelnascimentocc@gmail.com>
2024-01-18 10:58:31 -0300

    add grid with OrderTypeView component

--- ChelfDelivery/GridView/OrderTypeGridView.swift
+++ ChelfDelivery/GridView/OrderTypeGridView.swift
@@ -8,21 +8,24 @@
 import SwiftUI
 
 struct OrderTypeGridView: View {
+    var gridLayout: [GridItem] {
+        return Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
+    }
     
     var body: some View {
-        LazyHGrid(rows: [
-            GridItem(.fixed(100)),
-            GridItem(.fixed(100))
-        ]){
+        LazyHGrid(rows: gridLayout, spacing: 15){
             ForEach(ordersMocks){
-                orderItem in Text(orderItem.name)
+                orderItem in OrderTypeView(orderType: orderItem)
             }
         }
+        .frame(height: 200)
+        .padding(.horizontal, 15)
+        .padding(.top, 15)
     }
 }
 
 struct OrderTypeGridView_Previews: PreviewProvider {
     static var previews: some View {
-        OrderTypeGridView()
+        OrderTypeGridView().previewLayout(.sizeThatFits)
     }
 }
--- ChelfDelivery/GridView/OrderTypeView.swift
+++ ChelfDelivery/GridView/OrderTypeView.swift
@@ -13,13 +13,18 @@ struct OrderTypeView: View {
     var body: some View {
         VStack(spacing: 5){
             Image(orderType.image)
+                .resizable()
+                .scaledToFit()
+                .fixedSize(horizontal: false, vertical: true)
             Text(orderType.name)
+                .font(.system(size: 10))
         }
+        .frame(width: 70, height: 100)
     }
 }
 
 struct OrderTypeView_Previews: PreviewProvider {
     static var previews: some View {
-        OrderTypeView(orderType: OrderType(id: 1, name: "Restaurante", image: "hamburguer"))
+        OrderTypeView(orderType: OrderType(id: 1, name: "Restaurante", image: "hamburguer")).previewLayout(.sizeThatFits)
     }
 }
